68fc07b450d07cabe6f8bbed99575ff1154ba9a2
mikigo <huangmingqiang@uniontech.com>
2023-06-01 13:17:37 +0800

    update

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 from allure_custom import AllureCustom
-
 from allure_custom.conf import setting
 
 # 配置你的static静态文件
 # setting.static = "~/static"
 # setting.logo_png = "logo.png"
 
-# # 同步你的配置
+# 同步你的配置
 AllureCustom.allure_custom()
+
 # 生成html测试报告
 AllureCustom.gen(report_path="~/Desktop/report", generate_allure_html="~/Desktop/html")
 
