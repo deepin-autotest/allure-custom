9bb45ecfbb59b03cb6c5194bb07d7d86e77a6e9f
mikigo <huangmingqiang@uniontech.com>
2023-06-26 17:26:52 +0800

    update

--- allure_custom/__version__.py
+++ allure_custom/__version__.py
@@ -1,3 +1,3 @@
 __title__ = "allure_custom"
 
-__version__ = "2023.6.26"
+__version__ = "2023.6.27"
--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@ mkdocs gh-deploy
 rm -rf ./site
 
 rm -rf ./dist
-rm -rf ./pdocr_rpc.egg-info
 
 python3 -m build
 twine upload dist/*
